8688c970e1335dc453e79edb16a16651b2be0fe5
Piotr <gonska9933@gmail.com>
2018-09-15 21:08:10 +0200

    Create required functions in database handler module which are handling register feature

--- data_handler.py
+++ data_handler.py
@@ -1,37 +1,28 @@
-import helper_functions as hf
-
-DIRECTORY = "data"
-FILE_NAME = "game.csv"
-ROWS = 4
-STARTING_PROGRESS = [0]*(ROWS*ROWS)
+import data.db_connection as dbc
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
-def read_file():
-    """
-    Read game progress from file and returns list of list of rows,
-    if file does not exist creates new file with STARTING PROGRESS data
-    """
+@dbc.connection_handler
+def get_player(cursor, player_name):
+    SQL = (f'SELECT 1 '
+           f'FROM players '
+           f"WHERE player_name='{player_name}';")
 
-    try:
-        with open(DIRECTORY+'/'+FILE_NAME, 'r') as file:
-            content = file.read().split(',')
-        content = list(hf.chunks(content, ROWS))
-        result = [[int(e) for e in row] for row in content]
-
-        return result
-    except FileNotFoundError:
-        with open(DIRECTORY+'/'+FILE_NAME, 'w+') as file:
-            file.write(','.join(str(e) for e in STARTING_PROGRESS))
-        return read_file()
+    cursor.execute(SQL, (player_name,))
+    result = cursor.fetchone()
+    return result
 
 
-def save_to_file(arg):
-    """
-    Takes list of lists of the game progress as argument and converts it to a list of strings of row to save in the file
-    """
+@dbc.connection_handler
+def add_player(cursor, registration_form):
+    player_name = registration_form.get('username')
+    if get_player(player_name) == None:
+        password = generate_password_hash(registration_form.get('password'))
+        
+        SQL = (f'INSERT INTO players (player_name, password) '
+               f"VALUES ('{player_name}', '{password}');")
 
-    unpacked = [str(item) for element in arg for item in element]
-    converted = ','.join(unpacked)
-
-    with open("%s/%s" % (DIRECTORY, FILE_NAME), 'w+') as file:
-        file.writelines(converted)
+        cursor.execute(SQL, (player_name, password))
+        return True
+    else:
+        return False
