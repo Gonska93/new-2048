0b925d2264f1138557165d8261e974939c5890ee
Szymon Nowak <sznowak13@gmail.com>
2018-10-22 14:23:22 +0200

    Deleted db_config

--- data/db_config.py
+++
@@ -1,6 +0,0 @@
-class PGSQL:
-    USER_NAME = 'sznowak13'
-    PASSWORD = 'koparka123'
-    HOST = 'localhost'
-    DB_NAME = 'new2048'
-    DBURI = f'postgresql://{USER_NAME}:{PASSWORD}@{HOST}/{DB_NAME}'
