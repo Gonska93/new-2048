1a581b07b70386c671381609ac09cd9e6249a6c2
Piotr <gonska9933@gmail.com>
2018-10-24 21:27:31 +0200

    chat feature

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, redirect, render_template, url_for, session, flash
+from flask_socketio import SocketIO
 from logic import user_handler as uh
 from util import GAME_MODES
 
 
 app = Flask(__name__)
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+socketio = SocketIO(app)
 
 
 @app.route('/')
@@ -45,10 +49,25 @@ def logout():
     return redirect(url_for('main'))
 
 
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+
+@socketio.on('user_connected')
+def handle_user_connection(json, methods=['GET', 'POST']):
+    json['message'] = session['player_name'] + ' has connected.'
+    print(json)
+    socketio.emit('my response', json, callback=messageReceived)
+
+
+@socketio.on('message')
+def handle_message(json, methods=['GET', 'POST']):
+    json['player_name'] = session['player_name']
+    socketio.emit('my response', json, callback=messageReceived)
+
+
 if __name__ == "__main__":
-    app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-    app.run(
-        host='127.0.0.1',
-        port=5000,
-        debug=True
-    )
+    socketio.run(app,
+                 host="127.0.0.1",
+                 port=5000,
+                 debug=True)
