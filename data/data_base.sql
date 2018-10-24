0d55e36bec0154939d74a7024ea0aaec775caf89
Szymon Nowakm <sznowak13@gmail.com>
2018-10-24 20:57:13 +0200

    Added save_title column to game table

--- data/data_base.sql
+++ data/data_base.sql
@@ -15,6 +15,7 @@ CREATE TABLE game (
     id serial NOT NULL,
     player_id integer NOT NULL,
     game_state integer[4][4] NOT NULL,
+    save_title varchar(30) NOT NULL,
     submission_time timestamp without time zone default date_trunc('second', localtimestamp)
 );
 
