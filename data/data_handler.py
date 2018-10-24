8f55b256351beb46f7e1866b3249ef736cca7706
Szymon Nowakm <sznowak13@gmail.com>
2018-10-24 20:58:14 +0200

    Added two new query functions
    for getting player id by username and for saving game states

--- data/data_handler.py
+++ data/data_handler.py
@@ -39,3 +39,20 @@ def get_all_usernames(cursor):
     cursor.execute(SQL)
     result = cursor.fetchall()
     return [row['player_name'] for row in result]
+
+
+@dbc.connection_handler
+def get_player_id_by_username(cursor, username):
+    sql = "SELECT id from players WHERE player_name=%s;"
+    cursor.execute(sql, (username,))
+    result = cursor.fetchone()
+
+    return result['id']
+
+
+@dbc.connection_handler
+def save_new_game(cursor, game_state, title,  player_id):
+    sql = ("INSERT INTO game(player_id, save_title, game_state) "
+           "VALUES (%(player_id)s, %(save_title)s, %(game_state)s);")
+
+    cursor.execute(sql, {'player_id': player_id, 'game_state': game_state, 'save_title': title})
