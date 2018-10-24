5e10857acae83faab08ccac3c5deef16388015d4
Szymon Nowakm <sznowak13@gmail.com>
2018-10-24 20:56:32 +0200

    Added new js module for ajax requests

---
+++ static/scripts/data_handler.js
@@ -0,0 +1,17 @@
+let dataHandler = {
+    saveData: function(board_data, title) {
+        fetch('/save-game', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
+            },
+            body: JSON.stringify({
+                game_state: board_data,
+                save_title: title
+            })
+        })
+        .then(response => response.json())
+        .then(result => flashResult(result))
+        .catch(err => console.log(err))
+    }
+}
