a20cba15393df3ac95c9d0e8f5ba70ae38702edc
Szymon Nowakm <sznowak13@gmail.com>
2018-10-24 20:56:32 +0200

    Added new js module for ajax requests

--- static/scripts/data_handler.js
+++ static/scripts/data_handler.js
@@ -13,11 +13,14 @@ let dataHandler = {
         .then(response => response.json())
         .then(result => flashResult(result))
         .catch(err => console.log(err))
+<<<<<<< HEAD
     },
     getSavedGames: function() {
         fetch('/get-saved-games')
         .then(response => response.json())
         .then(data => displaySavedGames(data.body))
         .catch(err => console.log(err))
+=======
+>>>>>>> Added new js module for ajax requests
     }
 }
