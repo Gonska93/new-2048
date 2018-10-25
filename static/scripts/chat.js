89204e3838580c50021d353083ceb7a247afccf4
Szymon Nowak <sznowak13@gmail.com>
2018-10-25 14:54:05 +0200

    modify chat js module

--- static/scripts/chat.js
+++ static/scripts/chat.js
@@ -1,22 +1,20 @@
-let socket = io.connect('http://' + document.domain + ':' + location.port);
-socket.on( 'connect', function() {
-    socket.emit( 'user_connected', {
-        message: ''
-    })
-        
-        
-let form = $( '#chat-input form' ).on( 'submit', 
-    function( e ) {
-            e.preventDefault()
-            socket.emit( 'message', {
-                message : document.getElementById('user-input').value
-                } )
+let socket = io.connect('http://' + document.domain + ':' + location.port),
+    form = $( '#chat-input form' );
 
-          $( '#user-input' ).val( '' ).focus()
-        } )
-      } )
+form.on( 'submit', ( e ) => {
+    e.preventDefault();
+    socket.emit( 'message', {
+        message : document.getElementById('user-input').value
+        } );
+    $( '#user-input' ).val( '' ).focus()
+});
 
-socket.on( 'my response', function( msg ) {
-        let messageToDisplay = (msg.player_name) ? `${msg.player_name}:${msg.message}`: msg.message
-        $('#chat-messages').append(`<div>${ messageToDisplay }</div>`)
-      })
+socket.on( 'connect', () => {
+    socket.emit( 'user_connected', { message: ''})
+});
+
+socket.on( 'response', ( msg ) => {
+    let messageToDisplay = (msg.player_name) ?
+        `${msg.player_name}:${msg.message}`: msg.message;
+    $('#chat-messages').append(`<div>${ messageToDisplay }</div>`)
+});
