88e2e72c3d7b67d102485712842ae32fc4cd2fdd
Piotr <gonska9933@gmail.com>
2018-11-05 20:03:48 +0100

    Add updateGameOverModal in dom module

--- static/scripts/dom.js
+++ static/scripts/dom.js
@@ -67,7 +67,13 @@ let dom = {
         loadButton.off('click');
         loadButton.on('click', dataHandler.getSavedGames);
         $('#savesDiv').replaceWith(loadButton);
-    }
+    },
 
-}
+    updateGameOverModal: function() {
+        alert(`You ${(gameplay.gameState == 1) ? 'won':'lost'}`);
+        gameplay.gameState = 0;
+        gameplay.started = false;
+        timer.stopTimer();
+    }
+};
 
